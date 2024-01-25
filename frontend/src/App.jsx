8db2aead816e4bb3e7afaa37f719d0ea2852dfd1
Chaitanya <chaitanyam187@gmail.com>
2024-01-25 20:07:52 +0530

    handle navigation of landing page requests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import Landing from "./pages/Landing";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route exact path="/" element={<Landing />}></Route>
         <Route exact path="/signup" element={<Signup />}></Route>
         <Route exact path="/signin" element={<Signin />}></Route>
         <Route exact path="/dashboard" element={<Dashboard />}></Route>
